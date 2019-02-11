d09fe0cb241ebae8d865f4dd10296453f15a3b75
George Haddad <george.haddad@postlight.com>
2019-02-11 12:21:34 +0200

    test: use mocha and chai to determine budget failure

---
+++ test/sitespeed.js
@@ -0,0 +1,31 @@
+const { expect, assert } = require('chai');
+
+describe('Sitespeed performance testing', async () => {
+  const tstamp = process.env.TSTAMP;
+  it('should recognize the sitespeed timestamp', async () => {
+    expect(tstamp).to.be.a('string');
+  });
+
+  describe('on a native speed network', async () => {
+    const nativeResults = require(`../sitespeed-result/${tstamp}/native/budgetResult.json`);
+    const failingTests = nativeResults.failing;
+
+    Object.keys(failingTests).forEach(page => {
+      const metrics = failingTests[page];
+
+      describe(`for page ${page}`, async () => {
+        metrics.forEach(mg => {
+          it(`should pass the metric ${mg.metric} with a max of ${
+            mg.limit
+          }`, async () => {
+            if (typeof mg.value === 'string') {
+              assert.isAtMost(parseFloat(mg.value), parseFloat(mg.limit));
+            } else {
+              assert.isAtMost(mg.value, mg.limit);
+            }
+          });
+        });
+      });
+    });
+  });
+});

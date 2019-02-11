7d2bd0ad54870c82e314a0cb4bae3f3f77bcb12a
George Haddad <george.haddad@postlight.com>
2019-02-11 12:48:43 +0200

    fix: some bugs

--- test/sitespeed.js
+++ test/sitespeed.js
@@ -1,28 +1,30 @@
 const { expect, assert } = require('chai');
 
-describe('Sitespeed performance testing', async => {
+describe('Sitespeed performance testing', () => {
   const tstamp = process.env.TSTAMP;
-  it('should recognize the sitespeed timestamp', async => {
+  it('should recognize the sitespeed timestamp', done => {
     expect(tstamp).to.be.a('string');
+    done();
   });
 
-  describe('on a native speed network', async => {
+  describe('on a native speed network', () => {
     const nativeResults = require(`../sitespeed-result/${tstamp}/native/budgetResult.json`);
     const failingTests = nativeResults.failing;
 
     Object.keys(failingTests).forEach(page => {
       const metrics = failingTests[page];
 
-      describe(`for page ${page}`, async => {
+      describe(`for page ${page}`, () => {
         metrics.forEach(mg => {
           it(`should pass the metric ${mg.metric} with a max of ${
             mg.limit
-          }`, async => {
+          }`, done => {
             if (typeof mg.value === 'string') {
               assert.isAtMost(parseFloat(mg.value), parseFloat(mg.limit));
             } else {
               assert.isAtMost(mg.value, mg.limit);
             }
+            done();
           });
         });
       });
